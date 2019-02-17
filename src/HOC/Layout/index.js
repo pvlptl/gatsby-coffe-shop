ea83df43d4a77ed468717eb9bad19879597f6884
Pavel PYtliuk <gbnk.rr@gmail.com>
2019-02-17 19:44:37 +0200

    fixes

--- src/HOC/Layout/index.js
+++ src/HOC/Layout/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import "../../styles/bootstrap.css"
+import './styles.css';
 
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
