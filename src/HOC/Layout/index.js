38daa4183a32083437458b4af8672ba794701233
Pavel PYtliuk <gbnk.rr@gmail.com>
2019-02-17 19:34:33 +0200

    bootstrap css

--- src/HOC/Layout/index.js
+++ src/HOC/Layout/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import "bootstrap/dist/css/bootstrap.min.css"
+import "../../styles/bootstrap.css"
 
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
